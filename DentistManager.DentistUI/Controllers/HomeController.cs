6c03a597978e94ebb5f45c322b098de6a3e5daf6
ahmed-ibrahem <blood_line1986@yahoo.com>
2014-02-21 22:22:14 +0200

    add DI
    Works Fine :)

--- DentistManager.DentistUI/Controllers/HomeController.cs
+++ DentistManager.DentistUI/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DentistManager.Domain.DAL.Abstract;
 
 namespace DentistManager.DentistUI.Controllers
 {
     public class HomeController : Controller
     {
+        ItestRepository testRepository;
+        public HomeController(ItestRepository _testRepository)
+        {
+            testRepository = _testRepository;
+        }
         public ActionResult Index()
         {
+            ViewBag.text1 = testRepository.GetTesting();
             return View();
         }
 
---
+++ DentistManager.Domain/DAL/Concrete/testRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DentistManager.Domain.DAL.Abstract;
+
+namespace DentistManager.Domain.DAL.Concrete
+{
+    public class testRepository :ItestRepository
+    {
+        public string GetTesting()
+        {
+            return "Hello ";
+        }
+    }
+}
